b00233c37e1a98d49eefe7869e6241eeca5d177f
Nguyen <nguyen1au@hotmail.com>
2017-10-12 23:23:08 +1000

    Added switch and picker

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,TextInput,Picker, Switch} from 'react-native';
 
 const styles = require('../includes/style');
 
@@ -7,13 +7,68 @@ export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Main',
   };
+  constructor (props){
+    super(props);
+    this.state={
+      text:'',
+      radius:'20km',
+      switchState:true,
+    };
+  }
+
   render() {
     //const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text>
-          Main GT Watcher Screen
-        </Text>
+        <View style={styles.rowContainer}>
+          <Text style={styles.columnText1}>
+            Service Status: Stopped
+          </Text>
+          <Text style={styles.columnText2}>
+            Need Help?
+          </Text>
+        </View>
+        <View style={styles.rowContainer}>
+          <Text>
+            Location: Doolandella
+          </Text>
+        </View>
+        <View style={styles.rowContainer}>
+          <TextInput style={styles.textInput}
+            value={this.state.text}
+            placeholder='Change search location'
+            onChangeText={(text)=>{
+              this.setState({text});
+          }}>
+          </TextInput>
+          <Picker
+            style={styles.radiusPicker}
+            selectedValue={this.state.radius}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({radius: itemValue});
+              console.log(itemValue);
+            }}>
+            <Picker.Item label="5km" value="5" />
+            <Picker.Item label="10km" value="10" />
+            <Picker.Item label="20km" value="20" />
+            <Picker.Item label="50km" value="50" />
+            <Picker.Item label="100km" value="100" />
+            <Picker.Item label="250km" value="250" />
+            <Picker.Item label="500km" value="500" />
+          </Picker>
+        </View>
+        <View style={styles.rowContainer}>
+          <Text>Wifi Only</Text>
+          <Switch
+            value={this.state.switchState}
+            onValueChange={(value)=>{
+            this.setState({switchState:value});
+          }}>
+
+        </Switch>
+        </View>
+
+
       </View>
     );
   }
